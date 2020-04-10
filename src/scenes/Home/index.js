1b7977ae55b6dc6a3c57b92c9c9e25b5ba8bbc4a
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-10 19:16:38 +0430

    Add initial Home

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import "./styles.css";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
