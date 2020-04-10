bf525f28f03cf9dbae3748db0662b7dc66731540
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-11 04:21:19 +0430

    Add RestaurantInfoTitle component

---
+++ src/scenes/Restaurant/components/RestaurantInfo/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+import RestaurantInfoTitle from "./components/RestaurantInfoTitle";
+
+class RestaurantInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        <RestaurantInfoTitle />
+
+      </div>
+    );
+  }
+}
+
+export default RestaurantInfo;
