6c944713033afafba297b86378eb6f20c5f7a859
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-11 03:58:45 +0430

    Add RestaurantLogo component

--- src/scenes/Restaurant/index.js
+++ src/scenes/Restaurant/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import "./styles.css";
+import RestaurantLogo from "./components/RestaurantLogo";
 
 class Restaurant extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <main class="restaurant">
+          <RestaurantLogo />
+        </main>
         <Footer />
       </div>
     );
