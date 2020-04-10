7d74d4d3e3cf1551a4fe38a1f8afa5b1660e4f1b
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-10 19:45:27 +0430

    Add state to Home search text inputs

--- src/scenes/Home/components/HomeIntro/components/HomeIntroSearch/index.js
+++ src/scenes/Home/components/HomeIntro/components/HomeIntroSearch/index.js
@@ -2,25 +2,47 @@ import React from "react";
 import "./styles.css";
 
 class HomeIntroSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foodName: "",
+      restaurantName: ""
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  // TODO: Use General Inputs 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     return (
       <div class="container home-search">
-        <form
-          action="/search"
-          class="home-search-form row justify-content-around align-items-center"
-        >
+        <form class="home-search-form row justify-content-around align-items-center">
           <div class="col-sm-9 search-text-inputs flex justify-content-around align-items-center">
             <input
               type="text"
-              name="food_name"
+              name="foodName"
               placeholder="نام غذا"
               class="loghme-input-text"
+              value={this.state.foodName}
+              onChange={this.handleInputChange}
             />
             <input
               type="text"
-              name="restaurant_name"
+              name="restaurantName"
               placeholder="نام رستوران"
               class="loghme-input-text"
+              value={this.state.restaurantName}
+              onChange={this.handleInputChange}
             />
           </div>
           <div class="col-sm-3 search-submit-input">
