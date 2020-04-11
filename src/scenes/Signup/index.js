369605c4a242f06f5e0510c09252dfc555ef154a
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-11 06:40:40 +0430

    Fix errors

--- src/scenes/Signup/index.js
+++ src/scenes/Signup/index.js
@@ -5,37 +5,37 @@ import "./styles.css";
 class Signup extends React.Component {
   render() {
     return (
-      <div class="signup-body">
-        <div class="auth flex-center flex-col">
-          <div class="auth-logo">
+      <div className="signup-body">
+        <div className="auth flex-center flex-col">
+          <div className="auth-logo">
             <img src={logo} alt="Logo" />
           </div>
           <input
             type="text"
             name="name"
             placeholder="نام و نام خانوادگی"
-            class="loghme-input-text"
+            className="loghme-input-text"
           />
           <input
             type="email"
             name="email"
             placeholder="ایمیل"
-            class="loghme-input-text"
+            className="loghme-input-text"
           />
           <input
             type="password"
             name="password"
             placeholder="رمز عبور"
-            class="loghme-input-text"
+            className="loghme-input-text"
           />
           <input
             type="submit"
             value="ثبت‌نام"
-            class="loghme-button loghme-button-style"
+            className="loghme-button loghme-button-style"
           />
           <p>
             قبلا ثبت‌نام کرده‌اید؟{" "}
-            <a href="./login.html" class="auth-link login-link">
+            <a href="./login.html" className="auth-link login-link">
               ورود
             </a>
           </p>
