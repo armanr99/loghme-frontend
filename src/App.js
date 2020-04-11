bc8a6972d86d9d8d4bca8d99d4cb91b0a4504c6e
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-11 05:50:33 +0430

    Add base Profile component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./scenes/Home";
 import Restaurant from "./scenes/Restaurant";
+import Profile from "./scenes/Profile";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
+        <Route path="/profile">
+            <Profile />
+          </Route>
           <Route path="/restaurant">
             <Restaurant />
           </Route>
