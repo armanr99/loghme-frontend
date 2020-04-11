4c0d99fa9af89f9d97ae6ac6cb81af38a3a668e6
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-11 06:31:09 +0430

    Add signup components

--- src/scenes/Signup/index.js
+++ src/scenes/Signup/index.js
@@ -1,10 +1,45 @@
 import React from "react";
+import logo from "../../assets/images/logo.png";
 import "./styles.css";
 
 class Signup extends React.Component {
   render() {
     return (
-      <div>
+      <div class="signup-body">
+        <div class="auth flex-center flex-col">
+          <div class="auth-logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <input
+            type="text"
+            name="name"
+            placeholder="نام و نام خانوادگی"
+            class="loghme-input-text"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="ایمیل"
+            class="loghme-input-text"
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="رمز عبور"
+            class="loghme-input-text"
+          />
+          <input
+            type="submit"
+            value="ثبت‌نام"
+            class="loghme-button loghme-button-style"
+          />
+          <p>
+            قبلا ثبت‌نام کرده‌اید؟{" "}
+            <a href="./login.html" class="auth-link login-link">
+              ورود
+            </a>
+          </p>
+        </div>
       </div>
     );
   }
