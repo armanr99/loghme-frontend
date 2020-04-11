b4d9b29c8fdbc061286315b00b5991c39ba14994
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-11 06:16:03 +0430

    Add profile tab components

--- src/scenes/Profile/index.js
+++ src/scenes/Profile/index.js
@@ -2,15 +2,16 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import ProfileInfo from "./components/ProfileInfo";
-import "./styles.css";
+import ProfileTabs from "./components/ProfileTabs";
 
 class Profile extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <main className="profile">
+        <main>
           <ProfileInfo />
+          <ProfileTabs />
         </main>
         <Footer />
       </div>
