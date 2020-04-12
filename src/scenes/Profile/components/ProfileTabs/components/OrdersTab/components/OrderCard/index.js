a6b7b20656f5c4750e5b54a1330de23ac28cfd35
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-12 23:30:24 +0430

    Add delivery state view to order

--- src/scenes/Profile/components/ProfileTabs/components/OrdersTab/components/OrderCard/index.js
+++ src/scenes/Profile/components/ProfileTabs/components/OrdersTab/components/OrderCard/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import PropTypes from "prop-types";
 import OrderInfo from "./components/OrderInfo";
+import convertToPersianDigits from "../../../../../../../../services/tools/convertToPersianDigits";
 import "./styles.css";
 
 class OrderCard extends React.Component {
@@ -24,6 +25,16 @@ class OrderCard extends React.Component {
   }
 
   render() {
+    let stateText = "مشاهده فاکتور";
+    if (this.props.order.state === "Delivering") stateText = "پیک در مسیر";
+    else if (this.props.order.state === "Searching")
+      stateText = "در جست‌و‌جوی پیک";
+
+    let stateClass = "factor-button";
+    if (this.props.order.state === "Delivering") stateClass = "in-delivery";
+    else if (this.props.order.state === "Searching")
+      stateClass = "delivery-searching";
+
     return (
       <div className="profile-order row">
         <Modal
@@ -36,17 +47,17 @@ class OrderCard extends React.Component {
           <OrderInfo order={this.props.order} />
         </Modal>
         <div className="col-lg-1 col-2 flex-center">
-          <span>{this.props.index}</span>
+          <span>{convertToPersianDigits(this.props.index)}</span>
         </div>
         <div className="col-lg-6 col-5 profile-order-restaurant-name flex-center">
           <span>{this.props.order.cart.items[0].restaurant.name}</span>
         </div>
         <div className="col-lg-5 col-5 delivery-button-container flex-center">
           <button
-            className="loghme-button loghme-button-style factor-button delivery-button"
+            className={`loghme-button loghme-button-style ${stateClass} delivery-button`}
             onClick={this.handleShowFactor}
           >
-            مشاهده فاکتور
+            {stateText}
           </button>
         </div>
       </div>
@@ -60,5 +71,6 @@ OrderCard.propTypes = {
   index: PropTypes.number.isRequired,
   order: PropTypes.shape({
     cart: PropTypes.shape({ items: PropTypes.array.isRequired }).isRequired,
+    state: PropTypes.string.isRequired,
   }).isRequired,
 };
