d26e6e38285bb71ea6ebdb010621d5a7b551fd64
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-12 16:06:13 +0430

    Add increment and decrement to cart

--- src/components/Cart/components/CartItemCard/index.js
+++ src/components/Cart/components/CartItemCard/index.js
@@ -1,8 +1,53 @@
 import React from "react";
-import "./styles.css";
 import convertToPersianDigits from "../../../../services/tools/convertToPersianDigits";
+import { error, success } from "../../../../services/toastify/configs";
+import { toast } from "react-toastify";
+import API from "../../../../services/api";
+import { connect } from "react-redux";
+import mapStateToProps from "../../../../services/redux/configs/userStateToProps";
+import "./styles.css";
 
 class CartItemCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddFood = this.handleAddFood.bind(this);
+    this.handleDeleteFood = this.handleDeleteFood.bind(this);
+  }
+
+  async handleAddFood() {
+    const data = {
+      foodName: this.props.cartItem.food.name,
+      restaurantId: this.props.cartItem.food.restaurant.id,
+    };
+
+    try {
+      const response = await API.post("/cart", data);
+      const user = response.data;
+      this.props.dispatch({ type: "SET_USER", user: user });
+      toast.success(success.ADD_TO_CART);
+    } catch (err) {
+      toast.error(error.INTERNAL);
+    }
+  }
+
+  async handleDeleteFood() {
+    const data = {
+      foodName: this.props.cartItem.food.name,
+      restaurantId: this.props.cartItem.food.restaurant.id,
+    };
+
+    try {
+      const response = await API.delete("/cart", {
+        data: data,
+      });
+      const user = response.data;
+      this.props.dispatch({ type: "SET_USER", user: user });
+      toast.success(success.REMOVE_FROM_CART);
+    } catch (err) {
+      toast.error(error.INTERNAL);
+    }
+  }
+
   render() {
     return (
       <div className="cart-item flex justify-content-around align-items-center">
@@ -11,9 +56,15 @@ class CartItemCard extends React.Component {
         </div>
         <div className="cart-item-info flex flex-col justify-content-center align-items-center">
           <div className="cart-item-count flex justify-content-around align-items-center">
-            <i className="glyph-icon flaticon-plus"></i>
+            <i
+              className="glyph-icon flaticon-plus"
+              onClick={this.handleAddFood}
+            ></i>
             <span>{convertToPersianDigits(this.props.cartItem.count)}</span>
-            <i className="glyph-icon flaticon-minus"></i>
+            <i
+              className="glyph-icon flaticon-minus"
+              onClick={this.handleDeleteFood}
+            ></i>
           </div>
           <div className="cart-item-price">
             <span>
@@ -29,4 +80,4 @@ class CartItemCard extends React.Component {
   }
 }
 
-export default CartItemCard;
+export default connect(mapStateToProps)(CartItemCard);
