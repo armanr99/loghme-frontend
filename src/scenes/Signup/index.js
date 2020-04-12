537cab52216852a191a54c1405404c7d450cb5f5
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-12 21:59:14 +0430

    Refactor signup

--- src/scenes/Signup/index.js
+++ src/scenes/Signup/index.js
@@ -2,12 +2,50 @@ import React from "react";
 import logo from "../../assets/images/logo.png";
 import "./styles.css";
 import { Link } from "react-router-dom";
+import validateEmail from "../../services/tools/validateEmail";
+import { error } from "../../services/toastify/configs";
+import { toast } from "react-toastify";
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const email = this.state.email;
+    const password = this.state.password;
+
+    if (!validateEmail(email)) {
+      toast.error(error.EMAIL);
+      return;
+    } else if (!email || !password) {
+      toast.error(error.EMPTY_FIELD);
+      return;
+    }
+  }
+
   render() {
     return (
       <div className="signup-body">
-        <div className="auth flex-center flex-col">
+        <form className="auth flex-center flex-col" onSubmit={this.handleSubmit}>
           <Link to="/">
             <div className="auth-logo">
               <img src={logo} alt="Logo" />
@@ -18,31 +56,38 @@ class Signup extends React.Component {
             name="name"
             placeholder="نام و نام خانوادگی"
             className="loghme-input-text"
+            value={this.state.name}
+            onChange={this.handleInputChange}
           />
           <input
             type="email"
             name="email"
             placeholder="ایمیل"
             className="loghme-input-text"
+            value={this.state.email}
+            onChange={this.handleInputChange}
           />
           <input
             type="password"
             name="password"
             placeholder="رمز عبور"
             className="loghme-input-text"
+            value={this.state.password}
+            onChange={this.handleInputChange}
           />
-          <input
-            type="submit"
-            value="ثبت‌نام"
+          <button
             className="loghme-button loghme-button-style"
-          />
+            onClick={this.handleSubmit}
+          >
+            ورود
+          </button>
           <p>
             قبلا ثبت‌نام کرده‌اید؟{" "}
             <Link to="/login" className="auth-link login-link">
               ورود
             </Link>
           </p>
-        </div>
+        </form>
       </div>
     );
   }
