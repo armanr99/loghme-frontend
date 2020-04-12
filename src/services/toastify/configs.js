8c40d62dc804ea6310afb0d5c83854f39a57a9ec
Arman Rostami <arman.rostami.999@gmail.com>
2020-04-12 05:01:08 +0430

    Add tostify

---
+++ src/services/toastify/configs.js
@@ -0,0 +1,8 @@
+export const error = {
+  EMPTY_FIELD: "تمام فیلد‌های ورودی نیازمند پرشدن هستند",
+  INTERNAL: "خطای داخلی"
+};
+
+export const success = {
+  CHARGE: "حساب شما با موفقیت شارژ شد"
+}
