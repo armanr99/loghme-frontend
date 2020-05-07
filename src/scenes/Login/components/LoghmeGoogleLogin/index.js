d2dd44d7ce4ef1cce1194b815dad507f30d2d56a
Arman Rostami <arman.rostami.999@gmail.com>
2020-05-07 07:00:49 +0430

    Add google login service

--- src/scenes/Login/components/LoghmeGoogleLogin/index.js
+++ src/scenes/Login/components/LoghmeGoogleLogin/index.js
@@ -1,20 +1,44 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
+import { toast } from "react-toastify";
+import {withRouter} from 'react-router-dom'
+import { GoogleLogin } from "react-google-login";
+import API from "../../../../services/api";
+import { error } from "../../../../services/toastify/configs";
 import "./styles.css";
 
-const responseGoogle = (response) => {
-  console.log(response);
-};
-
 class LoghmeGoogleLogin extends React.Component {
+  responseGoogleSuccess = async (response) => {
+    if (response.getAuthResponse) {
+      const googleToken = await response.getAuthResponse().id_token;
+  
+      try {
+        const response = await API.post("/login/google", { token: googleToken });
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        window.location.href = "/";
+      } catch (err) {
+        if (err.response && err.response.status === 403) {
+          toast.error(error.UNREGISTERED_EMAIL);
+          this.props.history.push("/signup");
+        } else {
+          toast.error(error.INTERNAL);
+        }
+      }
+    }
+  };
+
+  responseGoogleError = async () => {
+    console.log("Google Error");
+  };
+
   render() {
     return (
       <div className="loghme-google-login">
         <GoogleLogin
           clientId="1055649283438-ehegliqteop5d72c91otdtpbouiiol3i.apps.googleusercontent.com"
           buttonText="ورود با گوگل"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
+          onSuccess={this.responseGoogleSuccess}
+          onFailure={this.responseGoogleError}
           cookiePolicy={"single_host_origin"}
         />
       </div>
@@ -22,4 +46,4 @@ class LoghmeGoogleLogin extends React.Component {
   }
 }
 
-export default LoghmeGoogleLogin;
+export default withRouter(LoghmeGoogleLogin);
